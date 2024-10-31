713e7d116738ad56aa11bd798d36c1bdcd4b5869
dominhit215@gmail.com <dominhit215@gmail.com>
2024-10-31 09:54:34 +0700

    initial

---
+++ src/main/java/com/gahoccode215/koifarmshop/KoifarmshopApplication.java
@@ -0,0 +1,13 @@
+package com.gahoccode215.koifarmshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoifarmshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoifarmshopApplication.class, args);
+	}
+
+}
